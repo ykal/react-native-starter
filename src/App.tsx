c8c823f50221afb7a26cef7cbffffb106e6bb67e
Kaleab Yitbarek <go4ykal@gmail.com>
2020-10-10 16:23:48 +0300

    start with stack navigation form app

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,19 @@
-import Home from './containers/Home';
-import Settings from './containers/Settings';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import * as React from 'react';
-import Icon from 'react-native-easy-icon';
-
-const Tab = createBottomTabNavigator();
+import {createStackNavigator} from '@react-navigation/stack';
+import Login from './containers/login';
 
 export type AppTabParamList = {
   Home: undefined;
   Settings: {userID?: string};
 };
 
+const Stack = createStackNavigator();
+
 const App = () => {
   return (
-    <Tab.Navigator
-      screenOptions={({route}) => ({
-        tabBarIcon: ({focused, color, size}) => {
-          let iconName = '';
-
-          if (route.name === 'Home') {
-            iconName = focused ? 'home' : 'home-outline';
-          } else if (route.name === 'Settings') {
-            iconName = focused ? 'settings' : 'settings-outline';
-          }
-
-          // You can return any component that you like here!
-          return <Icon name={iconName} type="material-community" size={size} color={color} />;
-        },
-      })}>
-      <Tab.Screen name="Home" component={Home} />
-      <Tab.Screen name="Settings" component={Settings} />
-    </Tab.Navigator>
+    <Stack.Navigator screenOptions={{headerShown: false}}>
+      <Stack.Screen name="Login" component={Login} />
+    </Stack.Navigator>
   );
 };
 
