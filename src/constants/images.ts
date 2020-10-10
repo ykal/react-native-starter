0a571d554cb45fe272d401d6ef31b3521ee7f99e
Kaleab Yitbarek <go4ykal@gmail.com>
2020-10-10 19:23:44 +0300

    add image assets

---
+++ src/constants/images.ts
@@ -0,0 +1,7 @@
+enum Images {
+  background = require('../../assets/images/background.png'),
+  flagEth = require('../../assets/images/flag-eth.png'),
+  logo = require('../../assets/images/logo.png'),
+}
+
+export default Images;
