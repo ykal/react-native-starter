b4c54201073e8080fd9035248ec5f5f27563a800
Kaleab Yitbarek <go4ykal@gmail.com>
2020-10-10 20:34:57 +0300

    refactor login screen

--- src/containers/login/index.tsx
+++ src/containers/login/index.tsx
@@ -2,27 +2,21 @@ import React from 'react';
 import {View, Image, TextInput} from 'react-native';
 import Images from '../../constants/images';
 
-import {CustomButton, CustomText} from '../../components';
+import {AuthScreenWrapper, CustomButton, CustomText} from '../../components';
 import styles from './styles';
 
 export default function Login() {
   return (
-    <View style={styles.container}>
-      <View style={styles.headerContainer}>
-        <Image source={Images.background} style={styles.logoBackground} />
-        <Image source={Images.logo} style={styles.logo} />
+    <AuthScreenWrapper>
+      <CustomText label="Login using your phone number" style={styles.captionText} />
+      <CustomText label="Let's Start" style={styles.title} />
+      <View style={styles.loginInput}>
+        <Image source={Images.flagEth} style={styles.flagIcon} />
+        <TextInput style={styles.input} placeholder="Phone Number" />
       </View>
-      <View style={styles.contentContainer}>
-        <CustomText label="Login using your phone number" style={styles.captionText} />
-        <CustomText label="Let's Start" style={styles.title} />
-        <View style={styles.loginInput}>
-          <Image source={Images.flagEth} style={styles.flagIcon} />
-          <TextInput style={styles.input} placeholder="Phone Number" />
-        </View>
-        <CustomText label={`By Pressing "submit" you agree to our`} style={styles.captionText} />
-        <CustomText label="terms & conditions" style={[styles.captionText, styles.linkText]} />
-        <CustomButton label="Submit" containerStyle={styles.loginButton} />
-      </View>
-    </View>
+      <CustomText label={`By Pressing "submit" you agree to our`} style={styles.captionText} />
+      <CustomText label="terms & conditions" style={[styles.captionText, styles.linkText]} />
+      <CustomButton label="Submit" containerStyle={styles.loginButton} />
+    </AuthScreenWrapper>
   );
 }
--- src/containers/login/styles.tsx
+++ src/containers/login/styles.tsx
@@ -2,33 +2,6 @@ import {StyleSheet} from 'react-native';
 import Colors from '../../constants/colors';
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  logoBackground: {
-    flex: 1,
-    width: null,
-    height: null,
-  },
-  headerContainer: {
-    flex: 0.6,
-    alignItems: 'stretch',
-    backgroundColor: Colors.WHITE,
-  },
-  logo: {
-    width: 150,
-    height: 150,
-    position: 'absolute',
-    left: '50%',
-    bottom: '25%',
-    marginLeft: -(150 / 2),
-  },
-  contentContainer: {
-    flex: 0.4,
-    backgroundColor: Colors.WHITE,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
   flagIcon: {
     width: 30,
     height: 15,
