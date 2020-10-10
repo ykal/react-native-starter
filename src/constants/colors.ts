57837cabc409765009fb6e84ec21f425c3f20913
Kaleab Yitbarek <go4ykal@gmail.com>
2020-10-10 15:59:08 +0300

    app colors enum

---
+++ src/constants/colors.ts
@@ -0,0 +1,8 @@
+export enum Colors {
+  PRIMARY_COLOR = '#F15A29',
+  SECONDARY_COLOR = '#238D6B',
+  WHITE = '#ffffff',
+  GREY = '#8A9EAD',
+  BLACK = '#000000',
+  BLACK_TRANSPARENT = 'rgba(0,0,0,0.2)',
+}
